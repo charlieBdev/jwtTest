56960c4366c57884639ddac5fd49f23524a43275
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-01-05 15:32:09 +0000

    add login route

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -48,4 +48,43 @@ router.post('/register', async (req, res) => {
 	}
 });
 
+// login
+router.post('/login', async (req, res) => {
+	try {
+		// 1 - destructure res.body
+		const { email, password } = req.body;
+
+		// 2 - check if user exists (if not, throw error)
+		const user = await pool.query(
+			'SELECT * FROM users WHERE user_email = $1;',
+			[email]
+		);
+
+		if (user.rows.length === 0) {
+			// 401 - unauthorised
+			// 403 - unauthenticated/forbidden
+			return res.status(403).json('Password or Email is incorrect');
+		}
+
+		// 3 - check if incoming pw matches decrypted db pw
+
+		const validPassword = await bcrypt.compare(
+			password,
+			user.rows[0].user_password
+		);
+
+		if (!validPassword) {
+			return res.status(403).json('Password or Email is incorrect');
+		}
+
+		// 4 - give them the/a jwt token
+		const token = jwtGenerator(user.rows[0].user_id);
+
+		res.json({ token });
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
