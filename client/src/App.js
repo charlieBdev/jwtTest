816c9f92ef0746d619376e314a17f307d1daad00
bigspambot <67501435+bigspambot@users.noreply.github.com>
2024-01-08 17:30:20 +0000

    add basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,44 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// components
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
-	return <Fragment></Fragment>;
+	/* eslint-disable no-unused-vars */
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	/* eslint-enable no-unused-vars */
+
+	return (
+		<>
+			<Router>
+				<div className='container'>
+					<Routes>
+						<Route
+							exact
+							path='/login'
+							// render={(props) => <Login {...props} />}
+							element={<Login />}
+						/>
+						<Route
+							exact
+							path='/register'
+							// render={(props) => <Register {...props} />}
+							element={<Register />}
+						/>
+						<Route
+							exact
+							path='/dashboard'
+							// render={(props) => <Dashboard {...props} />}
+							element={<Dashboard />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	return (
+		<>
+			<h1>Dashboard</h1>
+		</>
+	);
 };
 
 export default Dashboard;
